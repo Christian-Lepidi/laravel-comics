3bc1f2bb1494f9476a5429639c66ddb4b0e67d07
Christian-Lepidi <christianlepidi@gmail.com>
2024-03-17 12:17:25 +0100

    error 404 on screen

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,36 +1,40 @@
-<header>
-  <ul>
-    <li>
-     <a href="{{route('characters')}}">CHARACTERS</a>
-    </li>
-    <li>
-     <a href="{{route('comics')}}">COMICS</a>
-    </li>
-    <li> 
-     <a href="{{route('movies')}}">MOVIES</a>
-    </li>
-    <li> 
-     <a href="{{route('tv')}}">TV</a>
-    </li>
-    <li> 
-     <a href="{{route('games')}}">GAMES</a>
-    </li>
-    <li> 
-     <a href="{{route('collectibles')}}">COLLECTIBLES</a>
-    </li>
-    <li> 
-     <a href="{{route('videos')}}">VIDEOS</a>
-    </li>
-    <li> 
-     <a href="{{route('fans')}}">FANS</a>
-    </li>
-    <li> 
-     <a href="{{route('news')}}">NEWS</a>
-    </li>
-    <li> 
-     <a href="{{route('shop')}}">SHOP</a>
-    </li> 
-        
-    
-  </ul>  
+<header class="container">
+ <div class="row">
+  <div class="col-3"></div>
+  <div class="col-6">
+   <ul>
+     <li>
+      <a @class(['link','active'=> Route::currentRouteName()=='characters']) href="{{route('characters')}}">CHARACTERS</a>
+     </li>
+     <li>
+      <a @class(['link','active'=> Route::currentRouteName()=='comics']) href="{{route('comics')}}">COMICS</a>
+     </li>
+     <li> 
+      <a  @class(['link','active'=> Route::currentRouteName()=='movies']) href="{{route('movies')}}">MOVIES</a>
+     </li>
+     <li> 
+      <a @class(['link','active'=> Route::currentRouteName()=='tv']) href="{{route('tv')}}">TV</a>
+     </li>
+     <li> 
+      <a @class(['link','active'=> Route::currentRouteName()=='games']) href="{{route('games')}}">GAMES</a>
+     </li>
+     <li> 
+     <a @class(['link','active'=> Route::currentRouteName()=='collectibles']) href="{{route('collectibles')}}">COLLECTIBLES</a>
+     </li>
+     <li> 
+      <a @class(['link','active'=> Route::currentRouteName()=='videos']) href="{{route('videos')}}">VIDEOS</a>
+     </li>
+     <li> 
+      <a @class(['link','active'=> Route::currentRouteName()=='fans']) href="{{route('fans')}}">FANS</a>
+     </li>
+     <li> 
+      <a @class(['link','active'=> Route::currentRouteName()=='news']) href="{{route('news')}}">NEWS</a>
+     </li>
+     <li> 
+      <a @class(['link','active'=> Route::currentRouteName()=='shop']) href="{{route('shop')}}">SHOP</a>
+     </li> 
+    </ul>
+  </div> 
+  <div class="col-3"></div>
+ </div> 
 </header>
