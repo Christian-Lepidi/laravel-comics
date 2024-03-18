89daec2ef537bcd50ed4d14b07ff41648b795ecc
Christian-Lepidi <christianlepidi@gmail.com>
2024-03-18 16:02:17 +0100

    feat: add. shop section

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -12,7 +12,28 @@
   <a href="#" class="more">LOAD MORE</a>
  </div> 
 </div>
-<div class="shop-section"></div>
+<div class="shop-section">
+    <div class="click-shop">
+     <img src="{{Vite::asset('resources/images/buy-comics-digital-comics.png')}}" class="shop-img" alt="">
+     <span class="shop-text">DIGITAL COMICS</span>
+    </div>
+    <div class="click-shop">
+     <img src="{{Vite::asset('resources/images/buy-comics-merchandise.png')}}" class="shop-img" alt="">
+     <span class="shop-text">DC MERCHANDISE</span>
+    </div>
+    <div class="click-shop">
+     <img src="{{Vite::asset('resources/images/buy-comics-subscriptions.png')}}" class="shop-img" alt="">
+     <span class="shop-text">SUBSCRIPTION</span>
+    </div>
+    <div class="click-shop">
+     <img src="{{Vite::asset('resources/images/buy-comics-shop-locator.png')}}" class="shop-img" alt="">
+     <span class="shop-text">COMIC SHOP LOCATOR</span>
+    </div>
+    <div class="click-shop">
+     <img src="{{Vite::asset('resources/images/buy-dc-power-visa.svg')}}" class="shop-img" alt="">
+     <span class="shop-text">DC POWER VISA</span>
+    </div>
+</div>
      
  
 @endsection
